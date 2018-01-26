f36fb8e171ca49ec5db4c5c29a1cf53d0660ab93
Rohit D <rodiwaa@gmail.com>
2018-01-26 20:24:51 +0530

    Implemented Google sign in Expo

---
+++ constants/app/Auth.js
@@ -0,0 +1,4 @@
+export const GOOGLE_CLIENT = {
+  IOS: '57266899333-dvnclmd5141e3ldtf5ilboapphcad8nd.apps.googleusercontent.com',
+  ANDROID: '57266899333-ttvagkqndd7i4fvs8l3ao603kc2362mo.apps.googleusercontent.com'
+}
