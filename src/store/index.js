2033500a19e54602b1fbfcd4980ed0aa580d9b55
Rohit D <rodiwaa@gmail.com>
2018-02-01 07:04:46 +0530

    Moved content to src folder, added redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import { Reducers } from '../reducers'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 export const store = createStore(
   Reducers,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk),
+  applyMiddleware(logger)
 )
