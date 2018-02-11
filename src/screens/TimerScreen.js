1b71858a399ede40d6440e0d51d889a0b74d8d86
Rohit D <rodiwaa@gmail.com>
2018-02-11 14:17:32 +0530

    Get timer list on login; Show selected timer details.

--- src/screens/TimerScreen.js
+++ src/screens/TimerScreen.js
@@ -14,8 +14,23 @@ export default class TimerScreen extends React.Component {
 
   toggleTimer = () => this.setState({ isTimerRunning: !this.state.isTimerRunning }) // TODO: temporary
 
-  showTime = () => {
-    const { hh, mm } = this.state
+  showTimerTitle () {
+    const { params } = this.props.navigation.state
+
+    if (params) {
+      return <Text> {params.title} </Text>
+    }
+  }
+
+  showTimerHHMM = () => {
+    const { params } = this.props.navigation.state
+    let { hh, mm } = this.state
+
+    if (params) {
+      hh = params.hh,
+      mm = params.mm
+    }
+    
     return <Text> {`${hh}:${mm}`} </Text>
   }
 
@@ -33,7 +48,8 @@ export default class TimerScreen extends React.Component {
   render() {
     return (
       <View style={commonStyles.view}>
-        { this.showTime() }
+        { this.showTimerTitle() }
+        { this.showTimerHHMM() }
         { this.showButton() }
       </View>
     )
