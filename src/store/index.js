5aec8dc60dc90115e13f2343de3a903f5fc27a78
Rohit D <rodiwaa@gmail.com>
2018-02-17 21:12:59 +0530

    Implemented redux persist to store fetch appdata from device

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
-import { Reducers } from '../reducers'
+import Reducers from '../reducers'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
