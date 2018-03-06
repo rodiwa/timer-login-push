5728fc660181d26c2f0ab2b90716f1f1768fcf46
Ro Diwa <rodiwaa@gmail.com>
2018-03-07 00:57:42 +0530

    Updating list screen styles

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import {
-  View, Text, Button
-} from 'react-native'
+import { View } from 'react-native'
 import { commonStyles } from '../common/styles'
 import { NavigationActions } from 'react-navigation'
 
 import { TIMER_MSGS } from '../constants/Strings'
 import { addNewTimerAction, selectTimerFromListAction } from '../actions/AppActions'
+import { Container, Content, Button, Body, List, ListItem, Text, Left, Right } from 'native-base'
+import { Grid, Col, Row } from 'react-native-easy-grid'
 
 class ListScreen extends React.Component {
   renderTimerList () {
@@ -24,19 +24,45 @@ class ListScreen extends React.Component {
       arrTimer.push(timers[timer])
     }
 
-    return arrTimer.map((timerDetails, idx) =><Button key={idx} title={timerDetails.title} onPress={()=>selectTimerFromListAction(timerDetails)} />)
+    return (
+      <List style={{ /*backgroundColor: 'yellow',*/ flexGrow: 1 }}>
+        { arrTimer.map((timerDetails, idx) => (
+          <ListItem
+            style={{ paddingTop: 20, paddingBottom: 20 }}
+            key={idx}
+            title={timerDetails.title}
+            onPress={()=>selectTimerFromListAction(timerDetails)}>
+            <Left><Text style={{ fontSize: 30 }}>{timerDetails.title}</Text></Left>
+            <Right><Text style={{ fontSize: 20 }}>{`${timerDetails.hours}:${timerDetails.minutes}`}</Text></Right>
+          </ListItem>
+         )
+        )}
+      </List>
+    )
   }
 
   renderAddNewTimerBtn () {
-    return <Button title={TIMER_MSGS.ADD_NEW} onPress={()=>this.props.addNewTimerAction()} />
+    return <Button title={TIMER_MSGS.ADD_NEW} onPress={()=>this.props.addNewTimerAction()}><Text>{ TIMER_MSGS.ADD_NEW }</Text></Button>
   }
 
   render () {
     return (
-      <View style={commonStyles.view}>
-        { this.renderTimerList() }
-        { this.renderAddNewTimerBtn() }
-      </View>
+      <Container>
+        <Content contentContainerStyle={{flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
+          <Grid>
+            <Row size={80} style={{ /*backgroundColor: 'cyan',*/ justifyContent: 'center', alignItems: 'flex-start', alignContent: 'stretch' }}>
+              <View style={{ padding: 25, flexDirection: 'row' }}>
+                { this.renderTimerList() }
+              </View>
+            </Row>
+            <Row size={20} style={{ /*backgroundColor: 'pink',*/ justifyContent: 'center', alignItems: 'center' }}>
+              <View>
+                { this.renderAddNewTimerBtn() }
+              </View>
+            </Row>
+          </Grid>
+        </Content>
+      </Container>
     ) 
   }
 }
