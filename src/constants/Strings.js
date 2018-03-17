901a338da61e8a997d8d6373297b8b1ab7734216
Ro Diwa <rodiwaa@gmail.com>
2018-03-17 17:18:27 +0530

    Reduce tab icon sizes.

--- src/constants/Strings.js
+++ src/constants/Strings.js
@@ -15,3 +15,33 @@ export const LOGIN_MSGS = {
 export const TIMER_MSGS = {
   ADD_NEW: 'Add New Timer'
 }
+
+// user messages
+export const USER_MSGS = {
+  // sign in
+  SIGN_IN: 'Sign In',
+  SIGN_IN_GOOGLE: 'Google',
+
+  // list screen
+  ADD_NEW_TIMER: 'Add A New Timer',
+
+  // add new timer screen
+  ADD_TIMER: 'Add Timer To List',
+  CANCEL: 'Cancel',
+  CLICK_TO_CHANGE_TIME: 'Click To Change Time',
+  ADD_NAME_OF_TIMER: 'Add Name Of Timer',
+  START_TIMER: 'Start Timer',
+  STOP_TIMER: 'Stop Timer',
+  EDIT: 'Change Time',
+
+  // settings screen
+  CONFIRM_LOGOUT: 'Sure you logging out?',
+  YES_LOGOUT: 'Yep! Log Me Out!',
+  NO_LOGOUT: 'Nope! Take Me Back!',
+
+  // timer complete
+  TIMER_COMPLETE: 'Your time(r) is up!',
+  DONE: 'Done'
+
+
+}
